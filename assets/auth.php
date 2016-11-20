b061f01c3aea49e248a7eecfb1cf7b935f8c12ed
FadhilAhsan <fadhilahsan2104@gmail.com>
2016-11-21 01:44:35 +0700

    Integrated twitter API to PHP
    Integrated with with J7mbo

---
+++ assets/auth.php
@@ -0,0 +1,10 @@
+<?php
+
+$auth = array(
+    'oauth_access_token' => "YOUR ACCESS TOKEN",
+    'oauth_access_token_secret' => "YOUR SECRET ACCESS TOKEN",
+    'consumer_key' => "YOUR CONSUMER KEY",
+    'consumer_secret' => "YOUR SECRET CONSUMER KEY"
+);
+
+?>
